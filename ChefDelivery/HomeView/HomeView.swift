9cc1c73b734e5a7c0191ac4873cae3b9452049e9
Yuri Cunha <cunha.ysc@gmail.com>
2023-12-15 19:52:40 -0300

    finishing homeView

--- ChefDelivery/HomeView/HomeView.swift
+++ ChefDelivery/HomeView/HomeView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct HomeView: View {
     @State private var isAnimating = false
     @State private var imageOffset: CGSize = .zero
+    @State private var buttonOffset: CGFloat = .zero
+    @State private var showSecondScreen = false
+    let buttonHeight: CGFloat = 80
     var body: some View {
         GeometryReader { geometry in
             ZStack {
@@ -34,7 +37,7 @@ struct HomeView: View {
                 
                 VStack {
                     Text("Chef Delivery")
-                        .font(.system(size: 40))
+                        .font(.system(size: 48))
                         .fontWeight(.heavy)
                         .foregroundStyle(.colorRed)
                         .opacity(isAnimating ? 1 : 0)
@@ -70,7 +73,70 @@ struct HomeView: View {
                                 })
                         )
                     
-                    
+                    ZStack {
+                        Capsule()
+                            .fill(.colorRed.opacity(0.2))
+                        Capsule()
+                            .fill(.colorRed.opacity(0.2))
+                            .padding(8)
+                        
+                        Text("Descubra mais")
+                            .font(.title2)
+                            .bold()
+                            .foregroundStyle(.colorRedDark)
+                            .offset(x: 20   )
+                        
+                        HStack {
+                            Capsule()
+                                .fill(.colorRed)
+                                .frame(width: buttonOffset + buttonHeight)
+                            
+                            Spacer()
+                            
+                        }
+                        
+                        HStack {
+                            ZStack {
+                                Circle()
+                                    .fill(.colorRed)
+                                Circle()
+                                    .fill(.colorRedDark)
+                                    .padding(8)
+                                Image(systemName: "chevron.right.2")
+                                    .font(.system(size: 24))
+                                    .bold()
+                                    .foregroundStyle(.white)
+                            }
+                            Spacer()
+                        }
+                        .offset(x: buttonOffset)
+                        .gesture(
+                            DragGesture()
+                                .onChanged({ gesture in
+                                    
+                                    if gesture.translation.width >= 0 && buttonOffset <= (geometry.size.width - buttonHeight) {
+                                        withAnimation(.easeInOut(duration: 0.25)) {
+                                            buttonOffset = gesture.translation.width
+                                        }
+                                    }
+                                })
+                                .onEnded({ _ in
+                                    if buttonOffset > (geometry.size.width - 60) / 2 {
+                                        showSecondScreen = true
+                                    } else {
+                                        withAnimation(.easeInOut(duration: 0.25)) {
+                                            buttonOffset = 0
+                                        }
+                                    }
+                                })
+                        )
+                       
+                        
+                    }
+                    .frame(width: geometry.size.width - 60,height: buttonHeight)
+                    .opacity(isAnimating ? 1 : 0)
+                    .offset(x: isAnimating ? 0 : 100)
+
                 }
             }
         }
@@ -78,6 +144,9 @@ struct HomeView: View {
             withAnimation(.easeIn(duration: 1.5)) {
                 isAnimating = true
             }
+        }
+        .fullScreenCover(isPresented: $showSecondScreen) {
+            ContentView()
         }
     }
 }
